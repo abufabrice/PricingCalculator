5be5c4f504d68bc2ecb2265b7d32580ab74b40a8
abufabrice <abufabrice@yahoo.com>
2025-05-22 03:46:39 +0200

    Update app_streamlit_full.py

--- app_streamlit_full.py
+++ app_streamlit_full.py
@@ -36,7 +36,7 @@ categories = {
 st.markdown(
     f"""
     <div style='background-color:#004d40;padding:20px;border-radius:10px;margin-bottom:20px;'>
-        <h1 style='color:white;margin:0;'>🚌 Ongoing Pricing Simulator</h1>
+        <h1 style='color:white;margin:0;'>Ongoing Pricing Simulator</h1>
         <p style='color:#c8e6c9;font-size:16px;margin:0;'>{"Simulate the software costs for your digital transformation in real-time" if not is_french else "Simulez vos coûts logiciels en temps réel"}</p>
     </div>
     """,
